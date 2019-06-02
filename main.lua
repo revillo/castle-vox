66c74a1144d33083f8250b243b87c777293ed1f7
Oliver Castaneda <olivver@gmail.com>
2019-06-02 14:09:45 -0400

    Post api support

--- main.lua
+++ main.lua
@@ -48,36 +48,37 @@ end
 
 function renderLayer(grid, z)
 
-  local layer = grid.layers[z];
+  local layer = grid.voxels[z];
   love.graphics.setBlendMode("replace", "premultiplied");  
+  local w, h = grid.width, grid.height;
 
   --Per Layer
+  --[[
   love.graphics.setCanvas(layer.gpu);
-  local w, h = grid.width, grid.height;
   for x = 1,w do
   for y = 1,h do
     
-    love.graphics.setColor(layer.cpu[x][y]);
+    love.graphics.setColor(layer[x][y]);
     love.graphics.rectangle("fill", x-1, y-1, 1, 1);
   end end
     
   love.graphics.setCanvas()
-  
+  ]]
   
   --Tiled
   love.graphics.setCanvas(grid.gpu);
   
-  --[[
+  
   for x = 1,w do
   for y = 1,h do
-    
-    love.graphics.setColor(layer.cpu[x][y]);
-    love.graphics.rectangle("fill", x-1.1, (z-1) * grid.height + y-1.1, 1.1, 1.1);
+    love.graphics.setColor(layer[x][y]);
+    love.graphics.rectangle("fill", x-1, (z-1) * grid.height + y-1, 1, 1);
    end end
-  ]]
   
-  love.graphics.setColor(1,1,1,1);
-  love.graphics.draw(layer.gpu, 0, (z-1) * 16);
+  
+ -- love.graphics.setColor(1,1,1,1);
+  --love.graphics.draw(layer.gpu, 0, (z-1) * 16);
+  
   love.graphics.setCanvas();
   love.graphics.setBlendMode("alpha");
   
@@ -94,19 +95,14 @@ function clearGrid(grid)
   grid.gpu:setWrap("clampzero", "clampzero");
   grid.gpu:setFilter("nearest", "nearest");
   
+  grid.voxels = {};
   
   for z = 1,grid.depth do
     
-    grid.layers[z] = {
-      gpu = love.graphics.newCanvas(grid.width, grid.height, {dpiscale=1}),
-      cpu = {}
-    };
-    
-    grid.layers[z].gpu:setWrap("clampzero", "clampzero");
-    grid.layers[z].gpu:setFilter("nearest", "nearest");
+    grid.voxels[z] = {};
     
     local size = grid.width * grid.height;
-    local layer = grid.layers[z].cpu;
+    local layer = grid.voxels[z];
     
     for x = 1,w do
       layer[x] = {};
@@ -345,6 +341,7 @@ function makeShaders()
     uniform int sampling;
     uniform vec2 viewportSize;
     
+    
     vec4 getSkyColor(vec3 direction) 
     {
       
@@ -383,7 +380,7 @@ function makeShaders()
            vec3 op, on;
            bool hit;
            
-          vec4 sample = trace(origin, direction, op, on, hit, 3);
+          vec4 sample = trace(origin, direction, op, on, hit, 1);
           
           //float weight = dot(direction, normal) * (1.0 - float(hit));
           float weight = dot(direction, normal);
@@ -394,7 +391,7 @@ function makeShaders()
           //Ambient Light
           color += envLight * weight  * (1.0 - float(hit));
           //Bounce Light
-          color += sample.rgb * weight * float(hit) * 0.75;
+          color += sample.rgb * weight * float(hit) * 0.5;
 
           weightSum += weight;
 
@@ -569,24 +566,6 @@ end
 
 local queryCanvas = love.graphics.newCanvas(1, 1, {dpiscale=1});
 
-function drawGrid2D(grid)
-  
-  love.graphics.setColor(1,1,1,1);
-  
-  for z = 0, grid.depth-1 do
-    
-    local x = z % 4;
-    local y = math.floor(z / 4);
-    
-    love.graphics.draw(grid.layers[z + 1].gpu, 24 + x * 100, 24 + y * 100, 0, 4, 4);
-    
-  end
-  
-  --love.graphics.draw(queryCanvas, 0, 0, 0, 10, 10);
-  
- --love.graphics.draw(grid.gpu, 0, 0, 0, 1, 1);
- 
-end
 
 function drawImg(img, box)
   
@@ -704,7 +683,7 @@ function updateScript(script, dt)
   for z = 1, size do
     
     r, g, b, a = script(love.timer.getTime(), x, z, y);
-    state.grid.layers[z].cpu[x][y] = {r, g, b, a};
+    state.grid.voxels[z][x][y] = {r, g, b, a};
     
   end end end
   
@@ -712,53 +691,13 @@ function updateScript(script, dt)
 
 end
 
-function updatePaint2D()
-  local mx, my = love.mouse.getPosition();
-  
-  
-  local ix, iy = (mx - 24) / 100, (my - 24)/100;
-    
-    if (ix >= 4 or iy >= 4) then
-      return;
-    end
-    
-    local z = math.floor(iy) * 4 + math.floor(ix) + 1;
-    
-    
-    local px, py = (ix - math.floor(ix)) * 25, (iy - math.floor(iy)) * 25;
-    px, py = math.floor(px) + 1, math.floor(py) + 1;
-    
-    if (px > 16 or py > 16) then
-      return;
-    end
-    
-  if (love.mouse.isDown(1)) then
-    
-    state.grid.layers[z].cpu[px][py] = {
-      1, 1, 1, 1
-    }
-    
-    renderLayer(state.grid, z);
-    
-  elseif (love.mouse.isDown(2)) then
-  
-    state.grid.layers[z].cpu[px][py] = {
-      0, 0, 0, 0
-    }
-    
-    renderLayer(state.grid, z);
-    
-  end
-    
-end
-
 function removeVoxel(grid, x, y, z)
   
-  if (not grid.layers[z]) or (not grid.layers[z].cpu[x]) or (not grid.layers[z].cpu[x][y]) then
-    return ;
+  if (not grid.voxels[z]) or (not grid.voxels[z][x]) or (not grid.voxels[z][x][y]) then
+    return;
   end
   
-  grid.layers[z].cpu[x][y] = {
+  grid.voxels[z][x][y] = {
     0, 0, 0, 0
   }
     
@@ -783,11 +722,12 @@ local ColorUtil = {
 
 function fillVoxel(grid, x, y, z, target)
   
-  if (not grid.layers[z]) or (not grid.layers[z].cpu[x]) or (not grid.layers[z].cpu[x][y]) then
-    return
+  if (not grid.voxels[z]) or (not grid.voxels[z][x]) or (not grid.voxels[z][x][y]) then
+    return;
   end
   
-  local vc = grid.layers[z].cpu[x][y];
+  
+  local vc = grid.voxels[z][x][y];
   
   if (vc[4] == 0) then
     return
@@ -811,13 +751,14 @@ end
 
 function addVoxel(grid, x, y, z) 
   
-  if (not grid.layers[z]) or (not grid.layers[z].cpu[x]) or (not grid.layers[z].cpu[x][y]) then
-    return ;
+  if (not grid.voxels[z]) or (not grid.voxels[z][x]) or (not grid.voxels[z][x][y]) then
+    return;
   end
+  
 
   local c = state.color;
   
-  grid.layers[z].cpu[x][y] = {
+  grid.voxels[z][x][y] = {
     c[1], c[2], c[3], c[4]
   }
     
@@ -879,10 +820,10 @@ function edit3D(mx, my, toolOveride)
   elseif (tool == "remove") then
     removeVoxel(grid, x, y, z);
   elseif (tool == "pick color") then
-    local pc = grid.layers[z].cpu[x][y];
+    local pc = grid.voxels[z][x][y];
     state.color = {pc[1], pc[2], pc[3], pc[4]};
   elseif (tool == "fill") then
-    local pc = grid.layers[z].cpu[x][y];
+    local pc = grid.voxels[z][x][y];
     local vc = {};
     ColorUtil.copy(vc, pc);
     if(ColorUtil.equals(vc, state.color)) then return end;
@@ -967,13 +908,59 @@ function client.update(dt)
   
 end
 
+function castle.postopened(post)
+  
+  --FromPost = true;
+  
+  state.grid.voxels = post.data.voxels;
+  state.options = post.data.options;
+  
+  renderAllLayers(state.grid);
+  
+end
+
+function postGrid(grid)
+
+  network.async(function()
+      castle.post.create {
+          message = 'Voxelize This',
+          media = 'capture',
+          data = {
+              voxels = grid.voxels,
+              options = state.options
+          }
+      }
+  end)
+
+end
+
 local renderSectionToggle = true;
 local editSectionToggle = true;
+local postSectionToggle = true;
 
 function castle.uiupdate()
 
     local clear = false;
+    local post = false;
+        
+    --postSectionToggle = ui.section('Post', {open = postSectionToggle}, function()
+      
+      post = ui.button("Post to Castle!");
     
+    --end);
+
+    
+    editSectionToggle = ui.section('Editing', {open = editSectionToggle}, function()
+      state.color[1] = ui.slider('r', state.color[1] * 255, 0, 255) / 255;
+      state.color[2] = ui.slider('g', state.color[2] * 255, 0, 255) / 255;
+      state.color[3] = ui.slider('b', state.color[3] * 255, 0, 255) / 255;
+      
+      state.tool = ui.radioButtonGroup('Tool', state.tool, {'add', 'remove', 'pick color', 'fill'});
+      
+      clear = ui.button("Start Over");
+    end)
+    
+        
     renderSectionToggle = ui.section('Rendering', {open = renderSectionToggle}, function() 
       
       state.options.showGrid = ui.checkbox("Show Grid", state.options.showGrid, {
@@ -993,16 +980,10 @@ function castle.uiupdate()
       ]]
      
     end)
-    
-    editSectionToggle = ui.section('Editing', {open = editSectionToggle}, function()
-      state.color[1] = ui.slider('r', state.color[1] * 255, 0, 255) / 255;
-      state.color[2] = ui.slider('g', state.color[2] * 255, 0, 255) / 255;
-      state.color[3] = ui.slider('b', state.color[3] * 255, 0, 255) / 255;
-      
-      state.tool = ui.radioButtonGroup('Tool', state.tool, {'add', 'remove', 'pick color', 'fill'});
-      
-      clear = ui.button("Clear All");
-    end)
+
+    if (post) then
+      postGrid(state.grid);
+    end
     
     if (clear) then
       clearGrid(state.grid);
